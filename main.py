4e2d7b5d5335e9695d0a0f97431737006dbb4702
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-10 15:57:25 +0300

    cross ready telebottoken deleted

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from telebot import TeleBot, types
 from functions import *
 
 
-TOKEN = '5924570455:AAE4JkWLXeaDuwGyLxvg5Cmg8ikE47BSJ'
 BOT_PATH ='t.me/gbl_8_191222_bot'
 
 
@@ -31,91 +30,86 @@ def calc_r(msg: types.Message):
      bot.send_message(chat_id=msg.from_user.id, text='/calc - Калькулятор \n /cross - Крестики/нолики \n /log - Логирование')
 
 
+
 @bot.message_handler(commands=['cross'])
-def calc(msg: types.Message):
-     field = [' '] * 9
-     turn = 'X'
-     chck = None
-     print_board(msg, field)
-     while chck is None and ' ' in field:
-          move(msg, turn, field)
-          print_board(msg, field)
-          chck = check_result(field)
-          if turn == 'X':
-               turn = 'O'
-          else:
-               turn = 'X'
-     end_game = 'deuce'
-     if chck is not None:
-          end_game = chck + ' WIN!'
-     bot.send_message(chat_id=msg.from_user.id, text=f'GameOver {end_game}')
-     bot.send_message(chat_id=msg.from_user.id, text='/calc - Калькулятор \n /cross - Крестики/нолики \n /log - Логирование')
+def cross(msg: types.Message):
+    global field
+    field = [' '] * 9
+    turn = 'x'
+    print_board(msg, field)
+    move(msg, field, turn)
+
+def move(msg, field, turn):
+    bot.send_message(chat_id=msg.from_user.id, text=f'turn {turn}')
+    coord = ['', '']
+    bot.send_message(chat_id=msg.from_user.id, text='input row number (from top)')
+    bot.register_next_step_handler(msg, inp_row, coord=coord, turn=turn)
+
+def inp_row(msg: types.Message, coord, turn):
+    #    global row
+    try:
+        coord[0] = int(msg.text)
+        bot.send_message(chat_id=msg.from_user.id, text='input position number (from left)')
+        bot.register_next_step_handler(msg, inp_pos, coord=coord, turn=turn)
+    except:
+        bot.send_message(chat_id=msg.from_user.id, text='wrong input, repeat please')
+        move(msg, field, turn)
+
+def inp_pos(msg: types.Message, coord, turn):
+    #    global pos
+    try:
+        coord[1] = int(msg.text)
+        if coord[1] < 1 or coord[1] > 3 or field[coord[0] * 3 - 3 + coord[1] - 1] != ' ' or coord[0] < 1 or coord[0] > 3:
+            bot.send_message(chat_id=msg.from_user.id, text='wrong input, repeat please')
+            move(msg, field, turn)
+        else:
+            field[coord[0] * 3 - 4 + coord[1]] = turn
+            check_result(msg, field, turn)
+    except:
+        bot.send_message(chat_id=msg.from_user.id, text='wrong input, repeat please')
+        move(msg, field, turn)
+
+def check_result(msg, field, turn):
+    for i in range(3):
+        if field[i * 3] == field[i * 3 + 1] == field[i * 3 + 2] and field[i * 3] != ' ':
+            bot.send_message(chat_id=msg.from_user.id, text=f'GameOver {field[i+3]} WIN!')
+            bot.send_message(chat_id=msg.from_user.id, text='/calc - Калькулятор \n /cross - Крестики/нолики \n /log - Логирование')
+            return
+        elif field[i] == field[i + 3] == field[i + 6] and field[i] != ' ':
+            bot.send_message(chat_id=msg.from_user.id, text=f'GameOver {field[i]} WIN!')
+            bot.send_message(chat_id=msg.from_user.id, text='/calc - Калькулятор \n /cross - Крестики/нолики \n /log - Логирование')
+            return
+        elif (field[0] == field[4] == field[8] or field[2] == field[4] == field[6]) and field[4] != ' ':
+            bot.send_message(chat_id=msg.from_user.id, text=f'GameOver {field[4]} WIN!')
+            bot.send_message(chat_id=msg.from_user.id, text='/calc - Калькулятор \n /cross - Крестики/нолики \n /log - Логирование')
+            return
+    tmp = len(list(filter(lambda x: x == 'x' or x == 'o', field)))
+    if tmp == 9:
+        bot.send_message(chat_id=msg.from_user.id, text=f'GameOver deuce')
+        bot.send_message(chat_id=msg.from_user.id, text='/calc - Калькулятор \n /cross - Крестики/нолики \n /log - Логирование')
+    else:
+        print_board(msg, field)
+        turn = change_turn(turn)
+        move(msg, field, turn)
 
 
-def move(msg, sign, field):
-    bot.send_message(chat_id=msg.from_user.id, text=f'turn {sign}')
-    row = 0
-    while (row < 1 or row > 3) or ' ' not in field[row * 3 - 3: row * 3]:
-         bot.send_message(chat_id=msg.from_user.id, text='input row number (from top)')
-         bot.register_next_step_handler(msg, inp_move)
-         row = inp_move(msg)
-    pos = 0
-    while pos < 1 or pos > 3 or field[row * 3 - 3 + pos - 1] != ' ':
-         bot.send_message(chat_id=msg.from_user.id, text='input position number (from left)')
-         pos = bot.register_next_step_handler(msg, lambda x: x)
-    field[row * 3 - 4 + pos] = sign
 
-def inp_move(msg):
-     row = int(msg.text)
 
+#@bot.message_handler()
 
 def print_board(msg, field):
-    hor_divider = '+---' * 3 + '+'
+    board = ''
     for i in range(0, 9, 3):
         j = i
-        hor_line = ''
         while j < i + 3:
-            hor_line += '| ' + field[j] + ' '
+            board += '| ' + field[j] + ' '
             j += 1
-        hor_line += '|'
-        bot.send_message(chat_id=msg.from_user.id, text=hor_divider)
-        bot.send_message(chat_id=msg.from_user.id, text=hor_line)
-    bot.send_message(chat_id=msg.from_user.id, text=hor_divider)
+        board += '|\n' + '-' * 13 + '\n'
+    board = board.rstrip('\n').rstrip('-').rstrip('\n')
+    bot.send_message(chat_id=msg.from_user.id, text=board)
 
 
 
 
-bot.polling()
+bot.infinity_polling()
 
-'''
-@bot.message_handler()
-def answer(msg: types.Message):
-     text = msg.text
-     if text == '+':
-          bot.register_next_step_handler(msg, answer1)
-          bot.send_message(chat_id=msg.from_user.id, text='Введите слагаемые')
-     elif text == '-':
-          bot.register_next_step_handler(msg, answer2)
-          bot.send_message(chat_id=msg.from_user.id, text='Введите уменьшаемое и вычитаемое')
-     else:
-          bot.send_message(chat_id=msg.from_user.id, text='Вы прислали: ' + msg.text +
-                                                          ', а должны были арифметическое действие')
-
-
-def answer1(msg):
-     a, b = map(int, msg.text.split())
-     bot.send_message(chat_id=msg.from_user.id, text=f'Результат сложения {a + b}')
-
-
-def answer2(msg):
-     a, b = map(int, msg.text.split())
-     bot.send_message(chat_id=msg.from_user.id, text=f'Результат вычитания {a - b}')
-
-
-
-def log(update: Update, context: CallbackContext):
- file = open('db.csv', 'a')
- file.write(f'{update.effective_user.first_name},{update.effective_user.id}, {update.message.text}\n')
- file.close()
- 
-'''
